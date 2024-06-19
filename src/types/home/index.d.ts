974da58bd39a82879a848d58bea315e6ac2bfdd9
zzydannyer <1404631613@qq.com>
2024-06-19 15:41:36 +0800

    feat: 更新首页通知接口定义

--- src/types/home/index.d.ts
+++ src/types/home/index.d.ts
@@ -12,7 +12,7 @@ interface Basic {
   uid: string;
   title: string;
   releaseDate: string;
-  isLink: string; // 0:否 1:是
+  isLink: '0' | '1'; // 0:否 1:是
   link: string;
 }
 interface TopicPic extends Basic {
@@ -32,34 +32,19 @@ interface HomeHeadNews extends Basic {
   picUrl: string;
 }
 
-interface HomeNotice extends Basic{
-    searchValue: null,
-    uid: 'b1ea4f5b9d71432292b01191d43390d3',
-    creatorUid: '82bab99a420f4fb3b8feaf5959d6b770',
-    createTime: '2023-12-05 14:16:52',
-    updaterUid: '82bab99a420f4fb3b8feaf5959d6b770',
-    updateTime: '2023-12-05 14:16:52',
-    params: {},
-    homePlate: null,
-    channelId: '6ffa10437a604e5abd45a6727f8801d6',
-    orgId: '00010001',
-    title: '近期意识形态工作提示',
-    releaseDate: '2023-12-05 14:16:19',
-    description: null,
-    homeLevel: 0,
-    picUrl: null,
-    isLink: '0',
-    link: null,
-    receiveType: '1',
-    recRoleType: '0',
-    status: '20',
-    type: '1',
-    creatorRealName: ' 系统管理员',
-    isActive: '1',
-    topicType: null,
-    channelName: '工作提示',
-    channelOrder: null,
-    sourceFrom: null,
-    topicName: null,
-    isRead: true
-  }
+interface HomeNotice extends Basic {
+  channelId: string; //所属栏目id
+  orgId: string;
+  receiveType: '0' | '1' | '2' | '3' | '9'; //接收单位类型( 0:关闭;1:全部;2:二级单位 3:三级单位 9:部分公司)
+  recRoleType: '0' | '1'; //角色接收类型 0 全部 1自定义
+  status: '20'; // 状态(0:草稿; [1:审核中,11:集团审核,12:二级机构审核,13:三级机构审核]
+  type: '1';
+  creatorRealName: ' 系统管理员';
+  isActive: '1';
+  topicType: null;
+  channelName: '工作提示';
+  channelOrder: null;
+  sourceFrom: null;
+  topicName: null;
+  isRead: true;
+}
